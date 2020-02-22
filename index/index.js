135ad34ea6ff8e6179c69ac501891a4420a0eae5
ZhangChuanmin <45162999+ZhangChuanmin@users.noreply.github.com>
2020-02-22 12:50:11 +0800

    Update index.js

--- index/index.js
+++ index/index.js
@@ -1,68 +1,83 @@
 
-window.onload=function(){
+window.onload = function () {
     //UnblockMusic Pro
-    document.getElementById("ump").addEventListener("click",function(){
+    document.getElementById("ump").addEventListener("click", function () {
         window.open("https://github.com/nining377/UnblockMusicPro_Xposed");
     });
-    document.getElementById("dump").addEventListener("click",function(){
+    document.getElementById("dump").addEventListener("click", function () {
         window.open("https://github.com/nining377/UnblockMusicPro_Xposed/releases");
     });
 
     //UnblockNeteaseMusic
-    document.getElementById("unm").addEventListener("click",function(){
+    document.getElementById("unm").addEventListener("click", function () {
         window.open("https://github.com/nondanee/UnblockNeteaseMusic");
     });
-    document.getElementById("dunm").addEventListener("click",function(){
+    document.getElementById("dunm").addEventListener("click", function () {
         window.open("https://github.com/nondanee/UnblockNeteaseMusic/releases");
     });
 
     //云村清洁工
-    document.getElementById("pncm").addEventListener("click",function(){
+    document.getElementById("pncm").addEventListener("click", function () {
         window.open("https://github.com/zjns/PureNeteaseCloudMusic-Xposed");
     });
-    document.getElementById("dpncm").addEventListener("click",function(){
+    document.getElementById("dpncm").addEventListener("click", function () {
         window.open("https://github.com/zjns/PureNeteaseCloudMusic-Xposed/releases");
     });
 
     //太极
-    document.getElementById("tc1").addEventListener("click",function(){
+    document.getElementById("tc1").addEventListener("click", function () {
         window.open("http://taichi-app.com/");
     });
-    document.getElementById("tc2").addEventListener("click",function(){
+    document.getElementById("tc2").addEventListener("click", function () {
         window.open("http://taichi.cool/");
     });
-    
+
     //EdXposed
-    document.getElementById("edxp").addEventListener("click",function(){
+    document.getElementById("edxp").addEventListener("click", function () {
         window.open("https://github.com/ElderDrivers/EdXposed");
         window.open("https://github.com/ElderDrivers/EdXposedManager");
     });
-    document.getElementById("dedxp").addEventListener("click",function(){
+    document.getElementById("dedxp").addEventListener("click", function () {
         window.open("https://github.com/ElderDrivers/EdXposed/releases");
     });
-    document.getElementById("dedxpm").addEventListener("click",function(){
+    document.getElementById("dedxpm").addEventListener("click", function () {
         window.open("https://github.com/ElderDrivers/EdXposedManager/releases");
     });
 
     //黑苹果
-    document.getElementById("bap").addEventListener("click",function(){
+    document.getElementById("bap").addEventListener("click", function () {
         window.open("https://github.com/tsingui/clover-efi");
     });
 
     //Magisk
-    document.getElementById("mg").addEventListener("click",function(){
+    document.getElementById("mg").addEventListener("click", function () {
         window.open("https://github.com/topjohnwu/Magisk");
         window.open("https://github.com/topjohnwu/MagiskManager");
     });
-    document.getElementById("dmg").addEventListener("click",function(){
+    document.getElementById("dmg").addEventListener("click", function () {
         window.open("https://github.com/topjohnwu/Magisk/releases");
     });
-    document.getElementById("dmgm").addEventListener("click",function(){
+    document.getElementById("dmgm").addEventListener("click", function () {
         window.open("https://github.com/topjohnwu/MagiskManager/releases");
     });
 
     //KMSpico
-    document.getElementById("kms").addEventListener("click",function(){
+    document.getElementById("kms").addEventListener("click", function () {
         window.open("https://github.com/charygao/KMSpico_v10.2.0");
+    });
+
+    //移动端点击菜单
+    document.getElementById("menu").addEventListener("click", function () {
+        if (getComputedStyle(navbar, null).display == "none") {
+            document.getElementById("navbar").style.display = "block";
+        } else {
+            document.getElementById("navbar").style.display = "none";
+        }
+    });
+
+    //导航栏被点击时隐藏
+    document.getElementById("navbar").addEventListener("click", function () {
+        if (getComputedStyle(head, null).display == "block")
+            document.getElementById("navbar").style.display = "none";
     });
 }
